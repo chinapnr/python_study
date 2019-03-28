ac687d9631033adb0a4e327525c7277f7e47521e
yijun_mbp15 <wingfish@gmail.com>
2019-03-28 08:28:30 +0800

    2019.3.28 修改目录结构

--- s01_计算缓存、优化算法和加速 Python 执行/code/nim_fib.py
+++
@@ -1,9 +0,0 @@
-import time
-from fib_nimpy import fib
-
-if __name__ == "__main__":
-    x = 30
-    start = time.time()
-    res = fib(x)
-    elapsed = time.time() - start
-    print("Py3+Nim Computed fib(%s)=%s in %0.8f seconds" % (x, res, elapsed))
